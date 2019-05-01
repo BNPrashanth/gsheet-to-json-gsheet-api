aa719b8ae861aa82e06ff5dc5c9fc102c9273d34
BNPrashanth <bnprashanth256@gmail.com>
2019-05-01 14:51:58 +0530

    structure added with basic code

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	u "gsheet-to-json-google-api/src/utils"
+)
+
+func main() {
+	// Strting the Application
+	u.GeneralLogger.Println("Starting Extracting Language Files from GoogleSheet - GoogleSheets API approach..")
+}
